d798bc1a5383b9eecc490652c349209af94ffe88
NeerajMehta15 <Neerajmehta15@hotmail.com>
2025-11-05 16:13:58 +0530

    config.py changes

---
+++ src/config.py
@@ -0,0 +1,16 @@
+import os
+
+#Base directory of the project
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+#Data paths
+RAW_DATA_PATH = os.path.join(BASE_DIR, "data", "raw", "insurance_claims.csv")
+PROCESSED_DATA_PATH = os.path.join(BASE_DIR, "data", "processed", "processed_data.csv")
+
+# Model path
+MODEL_PATH = os.path.join(BASE_DIR, "models", "xgb_model.pkl")
+
+# Training parameters
+TEST_SIZE = 0.2
+RANDOM_STATE = 42
+TARGET_COL = "fraud_reported"
